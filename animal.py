d063ea1fed5c787136eefcea2b82e727b110b936
cch <scott19880525@gmail.com>
2022-05-09 22:38:41 +0800

    add cat.py and animal.py add name==main

--- animal.py
+++ animal.py
@@ -1,19 +1,20 @@
 class Animal:
-	def __init__(self, name):
-		self.name = name
+    def __init__(self, name):
+        self.name = name
 
-	def animal_name(self):
-		print(self.name)
-	def eat(self):
-		print("Animal eat method is called.")
+    def animal_name(self):
+        print(self.name)
+    def eat(self):
+        print("Animal eat method is called.")
 
 class Bird(Animal):
-	def fly(self):
-		print("Bird fly method is called.")
+    def fly(self):
+        print("Bird fly method is called.")
 
 class Duck(Bird):
-	def call(self):
-		print("quack")
+    def call(self):
+        print("quack")
 
-duck = Bird('little_duck')
-duck.fly()
+if __name__ == '__main__':
+    duck = Bird('little_duck')
+    duck.fly()
---
+++ cat.py
@@ -0,0 +1,10 @@
+from animal import Animal
+
+class Cat(Animal):
+    def call(self):
+        print('Meow~')
+
+
+if __name__ == '__main__':
+    black = Cat('little_cat')
+    black.call()
